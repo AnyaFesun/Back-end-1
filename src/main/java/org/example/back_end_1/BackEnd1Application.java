33a5da5568c21045bb49437d7eec2259dd99657e
Anna <anna.fesun825@gmail.com>
2024-10-31 19:05:20 +0200

    Initial commit

---
+++ src/main/java/org/example/back_end_1/BackEnd1Application.java
@@ -0,0 +1,13 @@
+package org.example.back_end_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEnd1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackEnd1Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/back_end_1/BackEnd1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.back_end_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEnd1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
